9a3becf0bf8e5f40e29c0b10e1ffb95f536d02c9
Kevin R. Thornton <krthornt@uci.edu>
2014-07-30 14:36:08 -0700

    starting new summary stat file

---
+++ src/SummStats.cc
@@ -0,0 +1,37 @@
+#include <Sequence/Ptable.hpp>
+#include <algorithm>
+#include <cctype>
+using namespace std;
+
+namespace Sequence
+{
+  unsigned Dij(const polymorphicSite & p, const std::vector< unsigned > & config, const unsigned & i, const unsigned & j)
+  {
+    unsigned rv = 0;
+    unsigned N = 0;
+    unsigned start1 = accumulate(config.begin(),config.begin()+i,0u),
+      start2 = accumulate(config.begin(),config.begin()+j,0u);
+    for( unsigned x = start1 ; x < start1 + config[i] ; ++x )
+      {
+	for(unsigned y = start2 ; y < start2 + config[j] ; ++y)
+	  {
+	    char ch1 = char(std::toupper(p.second[x])),ch2=char(std::toupper(p.second[y]));
+	    if(ch1 != 'N' && ch2 != 'N')
+	      {
+		rv += (ch1 != ch2) ? 1u : 0u;
+	      }
+	    else
+	      {
+		++N;
+	      }
+	  }
+      }
+    return rv - N;
+  }
+  
+  double Gmin(const Ptable & pt, const std::vector< unsigned > & config)
+  {
+    unsigned mdxy = numeric_limits<unsigned>::max(),sumdxy=0;
+  }
+
+}//ns Sequence
