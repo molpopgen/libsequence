06ad23258973217373b61c7dbc03e581615c467e
Kevin R. Thornton <krthornt@uci.edu>
2014-12-01 12:13:42 -0800

    more unique temp file name, clean up after self

--- test/FastaIO.cc
+++ test/FastaIO.cc
@@ -4,18 +4,22 @@
 #include <Sequence/Fasta.hpp>
 #include <fstream>
 #include <boost/test/unit_test.hpp>
+#include <unistd.h>
 
 std::string name("seqname"),seq("AGCGTAGACAGTAGAGTGAT");
 
 BOOST_AUTO_TEST_CASE( ostream_test )
 {
+  const char * filename = "fasta_ostream_test_out.fasta";
   Sequence::Fasta f(name,seq),f2;
-  std::ofstream o("testout.fasta");
+  std::ofstream o(filename);
   o << f << std::endl;
   o.close();
-  std::ifstream in("testout.fasta");
+  std::ifstream in(filename);
   in >> f2 >> std::ws;
   BOOST_REQUIRE(f==f2);
+  
+  unlink(filename);
 }
 
 //EOF
