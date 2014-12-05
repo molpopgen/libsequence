2ceaec4c4b97b26e46e6f28af79cd37f9e915ed2
Kevin R. Thornton <krthornt@uci.edu>
2014-12-04 17:24:50 -0800

    appear to have hit a minimal implementation of read compatible with multiple input methods.

--- src/Seq/Fasta.cc
+++ src/Seq/Fasta.cc
@@ -28,16 +28,17 @@ long with libsequence.  If not, see <http://www.gnu.org/licenses/>.
 namespace Sequence
 {
   /*! \example baseComp.cc */
-  Fasta::Fasta (const std::string &name, const std::string &seq):
+  Fasta::Fasta (const std::string &name, const std::string &seq) :
     Seq(name.c_str(),seq.c_str())
-	       /*!
-		 constructor for const std::string
-	       */
+    /*!
+      constructor for const std::string
+    */
   {}
-  Fasta::Fasta (const char *name,const char *seq):Seq(name,seq)
-	       /*!
-		 constructor for const char *
-	       */
+  Fasta::Fasta (const char *name,const char *seq) : 
+    Seq(name,seq)
+    /*!
+      constructor for const char *
+    */
   {}
   
   Fasta::Fasta (const Seq & seq) : Seq(seq) 
@@ -45,54 +46,24 @@ namespace Sequence
   {}
 
   std::istream & Fasta::read (std::istream & stream) 
-
   {
-    char ch;
-    bool seqflag;
-
-    if (!(stream >> ch))
-      {
-        stream.setstate (std::ios::badbit);
-        return (stream);
-      }
-    else
-      stream.putback (ch);
-
     std::string temp;
-
-    stream >> ch;
-
-    if (ch != '>')
+    int ch = stream.peek();
+    if( stream.eof() ) { return stream; }
+    if (char(ch) != '>')
       {
         throw badFormat("Fasta.cc: error, file not in FASTA format");
       }
+    //Read in name
+    std::getline(stream,first);
+    stream >> std::ws;
     first.clear();
-    while (1)
-      {
-        stream.get (ch);
-        if (ch == '\n')
-          break;
-	first += ch;
-      }
-    seqflag = 1;
     second.clear();
     second.reserve(1000);
-    while (seqflag)
+    while( char( ch = stream.peek() ) != '>' && ! stream.eof() )
       {
-        stream >> ch;
-        if (ch == '>')
-          {
-            stream.putback (ch);
-            seqflag = 0;
-          }
-        else if (stream.eof ())
-          seqflag = 0;
-        else
-          {
-	    second += ch;
-	    std::getline(stream,temp);
-	    second += temp;
-          }
+	std::getline(stream,temp);
+	second += temp;
       }
     return (stream);
   }
