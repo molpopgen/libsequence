c699a4da3e892f476283531687522025ed43845b
Kevin Thornton <kevin@Kevins-MacBook-Pro-4.local>
2014-12-15 10:12:25 -0800

    replaced enums with enum classes.  Removed enum types that led to weirdness is the divergence code base

--- Sequence/SeqEnums.hpp
+++ Sequence/SeqEnums.hpp
@@ -23,7 +23,7 @@ long with libsequence.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef SEQENUMS_H
 #define SEQENUMS_H
-
+#include <cstdint>
 /*! \file SeqEnums.hpp
   Defines a handfull of enumeration types useful
   for sequence data.
@@ -32,22 +32,16 @@ long with libsequence.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace Sequence
   {
-    /*! \enum Sequence::Nucleotides
-      An enum type for nucleotide data.  Comes in handy when you need
-      to iterate over all possible bases, etc.
-      enum values are:A=0,T,G,C,N,GAP
-    */
-    enum Nucleotides {A,T,G,C,N,GAP};
     /*! \enum Sequence::GeneticCodes
-      Only UNIVERSAL (=0)  is currently supported.
+      Only UNIVERSAL (= 0)  is currently supported.
       The order of the genetic codes is that of NCBI's code tables, available at 
       http://www.ncbi.nlm.nih.gov/htbin-post/Taxonomy/wprintgc?mode=c#SG2\n
     */
-    enum GeneticCodes{UNIVERSAL};
+    enum class GeneticCodes : std::int16_t {UNIVERSAL};
     /*! \enum Sequence::Mutations
       Values: Unknown=0,Ts, and Tv.\n
       Unknown means unknown, Ts means transition, Tv means transversion
     */
-    enum Mutations{Unknown,Ts,Tv};
+    enum class Mutations : std::int8_t {Unknown,Ts,Tv};
 }
 #endif
