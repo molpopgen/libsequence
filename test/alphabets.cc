d035f3f39d2fbcf557c754eff355bacea250aa92
Kevin R. Thornton <krthornt@uci.edu>
2014-12-15 14:25:22 -0800

    added

---
+++ test/alphabets.cc
@@ -0,0 +1,26 @@
+/*! \file alphabets.cc Unit tests for Sequence/SeqAlphabets.hpp */
+#define BOOST_TEST_MODULE alphabets
+#define BOOST_TEST_DYN_LINK 
+
+#include <Sequence/SeqAlphabets.hpp>
+#include <boost/test/unit_test.hpp>
+#include <algorithm>
+#include <iterator>
+
+BOOST_AUTO_TEST_CASE( check_dna_alphabet )
+{
+  for ( auto c : {'A','G','C','T'} )
+    {
+      BOOST_REQUIRE( std::distance(Sequence::dna_alphabet.begin(),
+				   std::find( Sequence::dna_alphabet.begin(),
+					      Sequence::dna_alphabet.end(), c ) ) < 4 );
+    }
+}
+
+BOOST_AUTO_TEST_CASE( check_isDNA )
+{
+  for (auto c : Sequence::dna_alphabet )
+    {
+      BOOST_REQUIRE( Sequence::isDNA(c) );
+    }
+}
