26785a2dbf2423fafe2f0df53216f03c12a24e82
Kevin R Thornton <krthornt@bs2-169-dhcp-173.bio.uci.edu>
2014-12-17 15:51:08 -0800

    more tests

--- test/PtableTest.cc
+++ test/PtableTest.cc
@@ -3,11 +3,13 @@
 #define BOOST_TEST_DYN_LINK 
 
 #include <Sequence/Ptable.hpp>
+#include <Sequence/PolySites.hpp>
 #include <Sequence/SeqAlphabets.hpp>
 #include <boost/test/unit_test.hpp>
 #include <cstdio>
 #include <cstdlib>
 #include <cctype>
+#include <iterator>
 #include <iostream>
 #include <algorithm>
 #include <functional>
@@ -30,4 +32,31 @@ BOOST_AUTO_TEST_CASE( ptable_remove_1 )
 			   } ),
 	   t.end() );
   BOOST_CHECK_EQUAL( t.size(), 1 );
+}
+
+BOOST_AUTO_TEST_CASE( ptable_make_from_polytable )
+{
+ using psite = Sequence::polymorphicSite;
+  Sequence::Ptable t = { psite(1.,"AAGC"),
+			 psite(2.,"ACAA") };
+
+  Sequence::PolySites ps(t.begin(),t.end());
+
+  BOOST_REQUIRE( std::distance(t.begin(),t.end()) ==
+		 std::distance(ps.sbegin(),ps.send()) );
+
+  auto t_i = t.begin();
+  auto ps_i = ps.sbegin();
+
+  while( t_i < t.end() )
+    {
+      BOOST_CHECK_EQUAL(t_i->first,ps_i->first);
+      BOOST_CHECK_EQUAL(t_i->second,ps_i->second);
+      ++t_i;
+      ++ps_i;
+    }
+
+  Sequence::Ptable t2(ps);
+
+  BOOST_REQUIRE( t == t2 );
 }
