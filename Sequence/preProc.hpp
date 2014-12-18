c4c2b8a9b30d61dffe90d9b130e54e60005e464e
Kevin R Thornton <krthornt@bs2-169-dhcp-173.bio.uci.edu>
2014-12-18 12:02:03 -0800

    added

---
+++ Sequence/preProc.hpp
@@ -0,0 +1,41 @@
+#ifndef __SEQUENCE_PREPROC_HPP__
+#define __SEQUENCE_PREPROC_HPP__
+
+#include <memory>
+#include <Sequence/Ptable.hpp>
+#include <Sequence/PolyTable.hpp>
+#include <Sequence/SummStats/Uhaps.hpp>
+#include <Sequence/stateCounter.hpp>
+
+namespace Sequence
+{
+  //! fwd declaration
+  struct preProcImpl;
+
+  class preProc
+  {
+  private:
+    std::unique_ptr<preProcImpl> __impl;
+  public:
+    mutable Ptable ptable;
+    mutable Uhaps uhaps;
+    using state_t = std::vector<stateCounter>;
+    using dstate_t = std::vector<std::pair<bool,stateCounter> >;
+    //! Unpolarized states per site.
+    mutable state_t states;
+    /*! 
+      Polarized states per site.		       
+      \note Will be empty if no info ancestral states are 
+      provided
+    */
+    mutable dstate_t dstates;
+  
+    preProc() = default;
+    preProc( const Ptable &, const bool & haveAncStates = 0, const std::string::size_type & anc = 0 );
+    preProc( Ptable &&, const bool & haveAncStates = 0, const std::string::size_type & anc = 0 );
+    preProc( const PolyTable &, const bool & haveAncStates = 0, const PolyTable::size_type & anc = 0 );
+  };
+}
+
+
+#endif
