807e55b227818992834de9c24347b0244c0b6afd
molpopgen <krthornt@uci.edu>
2014-12-18 05:45:12 -0800

    more messing around...

--- src/SummStats/preProc.cc
+++ src/SummStats/preProc.cc
@@ -19,13 +19,15 @@
   template base class to enforce some things?
 
   Maybe too complex?
- */
+
+  Take a cue from the STL and have "tags".  This forces all Summ stats to be templates, though...
+*/
 namespace Sequence {
 
   /*
-  struct Uhapvec
-  {
-  };
+    struct Uhapvec
+    {
+    };
   */
   
   struct preProcImpl
@@ -37,86 +39,96 @@ namespace Sequence {
 
     //constructors
     preProcImpl();
-    preProcImpl( const Ptable & );
-    preProcImpl( Ptable && );
-    preProcImpl( const PolyTable & );
-  };
+    preProcImpl( const Ptable & , const bool haveAncStates = 0, const std::string::size_type ancState = 0 );
+    preProcImpl( Ptable && , const bool haveAncStates = 0, const std::string::size_type ancState = 0 );
+    preProcImpl( const PolyTable & , const bool haveAncStates = 0, const PolyTable::size_type ancState = 0 );
+};
 
-  preProcImpl::preProcImpl() : pt( Ptable() )
-  {
-  }
+preProcImpl::preProcImpl() : pt( Ptable() )
+{
+}
   
-  preProcImpl::preProcImpl( const Ptable & __pt ) : pt(__pt)
-  {
-  }
+preProcImpl::preProcImpl( const Ptable & __pt, const bool haveAncStates, const std::string::size_type ancState ) : pt(__pt)
+{
+}
 
-  preProcImpl::preProcImpl( Ptable && __pt) : pt(std::move(__pt))
-  {
-  }
+preProcImpl::preProcImpl( Ptable && __pt, const bool haveAncStates, const std::string::size_type ancState) : pt(std::move(__pt))
+{
+}
 
-  preProcImpl::preProcImpl( const PolyTable & __pt) : pt(__pt)
-  {
-  }
+preProcImpl::preProcImpl( const PolyTable & __pt, const bool haveAncStates, const std::string::size_type ancState) : pt(__pt)
+{
+}
 
-  class preProc //: public std::pair< Ptable, Uhapvec >
-  {
-  private:
-    std::unique_ptr<preProcImpl> __impl;
-  public:
-    using hap_iterator = std::vector<std::vector<std::string>::iterator >::iterator;
-    using hap_const_iterator = std::vector<std::vector<std::string>::iterator >::const_iterator;
-    using hap_reference = std::vector<std::vector<std::string>::iterator >::reference;
-    using hap_const_reference = std::vector<std::vector<std::string>::iterator >::const_reference;
-    using site_iterator = Ptable::iterator;
-    using site_const_iterator = Ptable::const_iterator;
-    preProc();
-    preProc( const Ptable & );
-    preProc( Ptable && );
-    preProc( const PolyTable & );
+class preProc //: public std::pair< Ptable, Uhapvec >
+{
+private:
+  std::unique_ptr<preProcImpl> __impl;
+public:
+  using hap_iterator = std::vector<std::vector<std::string>::iterator >::iterator;
+  using hap_const_iterator = std::vector<std::vector<std::string>::iterator >::const_iterator;
+  using hap_reference = std::vector<std::vector<std::string>::iterator >::reference;
+  using hap_const_reference = std::vector<std::vector<std::string>::iterator >::const_reference;
+  using site_iterator = Ptable::iterator;
+  using site_const_iterator = Ptable::const_iterator;
 
-    //member functions
-    site_iterator begin();
-    site_iterator end();
-    hap_iterator hap_begin();
-    hap_iterator hap_end();
-    hap_const_iterator hap_begin() const;
-    hap_const_iterator hap_end() const;
-    hap_const_iterator hap_cbegin() const;
-    hap_const_iterator hap_cend() const;
-  };
-
-  preProc::preProc() : __impl(new preProcImpl())
-  {
-  }
+  //! Unpolarized states per site.
+  mutable std::vector<stateCounter> states;
+  /*! 
+    Polarized states per site.		       
+    \note Will be empty if no info ancestral states are 
+    provided
+  */
+  mutable std::vector<stateCounter> dstates;
   
-  preProc::preProc( const Ptable & __pt) : __impl(new preProcImpl(__pt))
-  {
-  }
+  preProc();
+  preProc( const Ptable & );
+  preProc( Ptable && );
+  preProc( const PolyTable & );
+
+  //member functions
+  site_iterator begin();
+  site_iterator end();
+  hap_iterator hap_begin();
+  hap_iterator hap_end();
+  hap_const_iterator hap_begin() const;
+  hap_const_iterator hap_end() const;
+  hap_const_iterator hap_cbegin() const;
+  hap_const_iterator hap_cend() const;
+};
+
+preProc::preProc() : __impl(new preProcImpl())
+{
+}
   
-  preProc::preProc( Ptable && __pt) : __impl(new preProcImpl(std::move(__pt)))
-  {
-  }
+preProc::preProc( const Ptable & __pt) : __impl(new preProcImpl(__pt))
+{
+}
   
-  preProc::preProc( const PolyTable & __pt ) : __impl(new preProcImpl(__pt))
-  {
-  }
+preProc::preProc( Ptable && __pt) : __impl(new preProcImpl(std::move(__pt)))
+{
+}
   
-  preProc::hap_iterator preProc::hap_begin() {
-    return __impl->haplotypes.begin();
-  }
-  preProc::hap_iterator preProc::hap_end() {
-    return __impl->haplotypes.end();
-  }
-  preProc::hap_const_iterator preProc::hap_begin() const {
-    return __impl->haplotypes.cbegin();
-  }
-  preProc::hap_const_iterator preProc::hap_end() const {
-    return __impl->haplotypes.cend();
-  }
-  preProc::hap_const_iterator preProc::hap_cbegin() const {
-    return __impl->haplotypes.cbegin();
-  }
-  preProc::hap_const_iterator preProc::hap_cend() const {
-    return __impl->haplotypes.cend();
-  }
+preProc::preProc( const PolyTable & __pt ) : __impl(new preProcImpl(__pt))
+{
+}
+  
+preProc::hap_iterator preProc::hap_begin() {
+  return __impl->haplotypes.begin();
+}
+preProc::hap_iterator preProc::hap_end() {
+  return __impl->haplotypes.end();
+}
+preProc::hap_const_iterator preProc::hap_begin() const {
+  return __impl->haplotypes.cbegin();
+}
+preProc::hap_const_iterator preProc::hap_end() const {
+  return __impl->haplotypes.cend();
+}
+preProc::hap_const_iterator preProc::hap_cbegin() const {
+  return __impl->haplotypes.cbegin();
+}
+preProc::hap_const_iterator preProc::hap_cend() const {
+  return __impl->haplotypes.cend();
+}
 }
