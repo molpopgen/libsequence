063eb1851bd31474d5630b68fc7a7bf9cb3d9683
Kevin R Thornton <krthornt@bs2-169-dhcp-173.bio.uci.edu>
2014-12-19 11:32:32 -0800

    doxygen stuff

--- Sequence/Poly8.hpp
+++ Sequence/Poly8.hpp
@@ -19,11 +19,35 @@ namespace Sequence
     //! vectors of itype
     using vtype = std::vector<itype>;
 
+    /*!
+      \return A vtype made from s
+     */
     vtype dna2vtype( const std::string & s );
+    /*!
+      \return A vtype made from s
+      \note Contents of s are cleared
+     */
     vtype dna2vtype( std::string & s );
+    /*!
+      \return A vtype made from s
+     */
     vtype dna2vtype( const Sequence::Seq & s );
+
+    /*!
+      \return A vtype made from s
+      \note Contents of s are cleared
+     */
     vtype dna2vtype( Sequence::Seq & s );
+
+    /*!
+      \return A std::string made from v
+    */
     std::string vtype2dna( const vtype & v );
+
+    /*!
+      \return A std::string made from v
+      \note Contents of v are cleared
+    */
     std::string vtype2dna( vtype & v );
   }
 }
