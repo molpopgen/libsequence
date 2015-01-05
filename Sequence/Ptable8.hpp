59a27f513881ecd83855861212c20fb009684e19
molpopgen <krthornt@uci.edu>
2015-01-04 17:07:29 -0800

    experiment with inheriting vectorizer

--- Sequence/Ptable8.hpp
+++ Sequence/Ptable8.hpp
@@ -1,6 +1,7 @@
 #ifndef __SEQUENCE_PTABLE8_HPP__
 #define __SEQUENCE_PTABLE8_HPP__
 
+#include <Sequence/util/vectorizer.hpp>
 #include <Sequence/Seq8.hpp>
 #include <Sequence/polySiteVector.hpp>
 #include <utility>
@@ -11,11 +12,11 @@ namespace Sequence
   using poly8site = std::pair<double,Seq8>;
   //fwd declarations
   class PolyTable;
-  class Ptable8 : private std::vector< poly8site >
+  class Ptable8 : public vectorizer<poly8site> 
   {
   private:
   public:
-    using base = std::vector< poly8site >;
+    using base = vectorizer<poly8site>;// std::vector< poly8site >;
     using base::base;
     //Constructors 
     Ptable8() = default;
@@ -24,50 +25,6 @@ namespace Sequence
     Ptable8( const PolyTable * );
     Ptable8( const std::vector<polymorphicSite> & );
     Ptable8( std::vector<polymorphicSite> & );
-
-    //import base types
-    using base::value_type;
-    using base::reference;
-    using base::const_reference;
-    using base::iterator;
-    using base::reverse_iterator;
-    using base::const_iterator;
-    using base::const_reverse_iterator;
-    using base::pointer;
-    using base::difference_type;
-    using base::size_type;
-    
-    //import base functions
-    using base::size;
-    using base::max_size;
-    using base::capacity;
-    using base::erase;
-    using base::clear;
-    using base::insert;
-    using base::swap;
-    using base::pop_back;
-    using base::reserve;
-    using base::shrink_to_fit;
-    using base::empty;
-    using base::resize;
-    using base::begin;
-    using base::end;
-    using base::rbegin;
-    using base::rend;
-    using base::crbegin;
-    using base::crend;
-    using base::cbegin;
-    using base::cend;
-    using base::operator[];
-    using base::operator=;
-    using base::at;
-    using base::push_back;
-    using base::emplace_back;
-    using base::emplace;
-    using base::front;
-    using base::back;
-    using base::data;
-    using base::assign;
   };
 }
 
