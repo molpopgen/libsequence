f03089e15dba434ee87038497e6ae465a93bc852
Kevin R. Thornton <krthornt@uci.edu>
2015-10-16 15:10:42 -0700

    l-HAP stat from doi:10.1371/journal.pgen.1005527.g001 added

---
+++ Sequence/SummStats/lHap.hpp
@@ -0,0 +1,12 @@
+#ifndef __SEQUENCE_SUMMSTATS_LHAP_HPP__
+#define __SEQUENCE_SUMMSTATS_LHAP_HPP__
+
+#include <Sequence/SimData.hpp>
+
+namespace Sequence 
+{
+  std::vector<double> lHap( const SimData & data, const double l );
+}
+
+
+#endif
---
+++ src/SummStats/lHap.cc
@@ -0,0 +1,30 @@
+#include <Sequence/SummStats/lHap.hpp>
+#include <algorithm>
+#include <numeric>
+
+namespace Sequence
+{
+  std::vector<double> lHap( const SimData & data, const double l )
+  {
+    //Get derived mutation frequency counts per site
+    std::vector<unsigned> dcounts;
+    std::for_each( data.sbegin(), data.send(),
+		   [&dcounts]( const polymorphicSite & p ) {
+		     dcounts.push_back( std::count(p.second.begin(),p.second.end(),'1') );
+		   } );
+    //Get the values for each element in the data
+    std::vector<double> rv(data.numsites(),0.);
+    for(unsigned i = 0 ; i < data.size() ; ++i )
+      {
+	auto j = std::find_if(data[i].cbegin(),data[i].cend(),[](const char & ch) {
+	    return ch == '1';
+	  });
+	while(j!=data[i].cend())
+	  {
+	    size_t d = j-data[i].cbegin();
+	    rv[d] += std::pow( double(dcounts[d]),2. );
+	  }
+      }
+    return rv;
+  }
+}
