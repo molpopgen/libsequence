6f3f244db1bb04a9619a5cc98c0ecf6cd06e146b
molpopgen <krthornt@uci.edu>
2015-10-24 20:45:00 +0200

    draf of lHaf_t

--- src/SummStats/lHaf.cc
+++ src/SummStats/lHaf.cc
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <numeric>
 #include <cmath>
+#include <thread>
 
 /*
   TODO:  A thread_site and thread_hap wrapper that apply some fxn to a site
@@ -15,6 +16,53 @@
  */
 namespace Sequence
 {
+  std::vector<double> lHaf_t( const SimData & data, const double l, const int nthreads )
+  {
+    std::vector<unsigned> dcounts(data.numsites());
+    
+    //std::function<void(const SimData::const_site_iterator& , unsigned &)>
+    const auto counter = [](const SimData::const_site_iterator& __s , unsigned & i ) {
+      i=unsigned(std::count(__s->second.begin(),__s->second.end(),'1'));
+    };
+    
+    for( auto i = data.sbegin() ; i != data.send() ; )
+      {
+	std::vector<std::thread> vt;
+	for(int j = 0 ; i != data.send() && j < nthreads ; ++i,++j)
+	  {
+	    vt.emplace_back(std::thread(std::cref(counter),
+					i,std::ref(dcounts[unsigned(i-data.sbegin())])));
+	  }
+	for( unsigned j = 0 ; j < unsigned(nthreads) ; ++j ) vt[j].join();
+      }
+    std::vector<double> rv(data.size());
+    for(auto i = data.cbegin() ; i != data.cend() ; )
+      {
+	std::vector<std::thread> vt;
+	for(int j = 0 ; i != data.end() && j < nthreads ; ++i,++j )
+	  {
+	    vt.emplace_back(std::thread([&rv,&dcounts,&j,&data,l](const SimData::const_data_iterator & __hap) {
+		  double rv_local = 0.;
+		  auto j = std::find_if(__hap->cbegin(),__hap->cend(),[](const char & ch) {
+		      return ch == '1';
+		    });
+		while(j != __hap->cend())
+		  {
+		    size_t d = size_t(j-__hap->cbegin());
+		    rv_local += std::pow( double(dcounts[d]),l );
+		    j = std::find_if(j+1,__hap->cend(),
+				     [](const char & ch) {
+				       return ch == '1';
+				     });
+		  }
+		rv[unsigned(__hap-data.begin())] = rv_local;
+		},i));
+	  } 
+      }
+    return rv;
+  }
+
+  
   std::vector<double> lHaf( const SimData & data, const double l )
   {
     //Get derived mutation frequency counts per site
