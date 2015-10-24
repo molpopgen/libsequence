f9c17e3a8a353418bed2d84db7b7975f5cae94f2
molpopgen <krthornt@uci.edu>
2015-10-24 20:45:53 +0200

    prototype

--- Sequence/SummStats/lHaf.hpp
+++ Sequence/SummStats/lHaf.hpp
@@ -10,6 +10,7 @@ namespace Sequence
     \ingroup popgenanalysis
   */
   std::vector<double> lHaf( const SimData & data, const double l );
+  std::vector<double> lHaf_t( const SimData & data, const double l, const int nthreads );
 }
 
 
