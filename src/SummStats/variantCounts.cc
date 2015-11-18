4853d15d9502dd432900e257d69077336737661e
Kevin R. Thornton <krthornt@uci.edu>
2015-11-18 14:52:17 -0800

    implemented constructor

--- src/SummStats/variantCounts.cc
+++ src/SummStats/variantCounts.cc
@@ -1,6 +1,15 @@
 #include <Sequence/SummStats/variantCounts.hpp>
 namespace Sequence
 {
+  variableSiteData::variableSiteData( double pos_, bool haveAnc,
+				      stateCounter counts_,stateCounter dcounts_) :
+    pos(pos_),
+    counts(std::move(counts_)),
+    dcounts(std::move(dcounts_)),
+    ancState(haveAnc)
+  {
+  }
+  
   template<>
   variantCounts<SimData> make_variantCounts(const SimData & sd, bool , unsigned, char  )
   {
