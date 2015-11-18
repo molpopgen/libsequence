808815644c5b60cbad3475d00105512b90c2c184
Kevin R. Thornton <krthornt@uci.edu>
2015-11-18 15:51:29 -0800

    new header file for classic summary stats

---
+++ Sequence/SummStats/classic.hpp
@@ -0,0 +1,12 @@
+/*!
+  "Classic" summaries of nucleotide variability
+ */
+
+#include <type_traits>
+#include <Sequence/SummStats/variantCounts.hpp>
+#include <Sequence/SimData.hpp>
+
+namespace Sequence
+{
+  
+}
