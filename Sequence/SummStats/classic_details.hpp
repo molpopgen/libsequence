c20c80758590b18bd1eeafe5e36ca089c0b9cbaf
Kevin R. Thornton <krthornt@uci.edu>
2015-11-18 16:49:18 -0800

    update protoype

--- Sequence/SummStats/classic_details.hpp
+++ Sequence/SummStats/classic_details.hpp
@@ -1,9 +1,16 @@
+#ifndef SEQUENCE_SUMMSTATS_CLASSIC_DETAILS_HPP
+#define  SEQUENCE_SUMMSTATS_CLASSIC_DETAILS_HPP
+
 #include <type_traits>
-#include <Sequence/SummStats/variantCounter.hpp>
+#include <Sequence/SummStats/variantCounts.hpp>
 
 namespace Sequence
 {
   namespace details
   {
+    double thetapi_details( const std::vector<variableSiteData> & c, unsigned nsam, std::true_type );
+    double thetapi_details( const std::vector<variableSiteData> & c, unsigned nsam, std::false_type );
   }
 }
+
+#endif
