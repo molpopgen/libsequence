d38dc0c36a9cd68419477bf79f27f80e2ddc9378
Kevin R. Thornton <krthornt@uci.edu>
2016-10-17 18:04:17 -0700

    draft a parallel version of H1,H12, etc.

--- src/SummStats/Garud.cc
+++ src/SummStats/Garud.cc
@@ -2,7 +2,10 @@
 #include <set>
 #include <string>
 #include <cmath>
+#include <numeric>
 #include <Sequence/SummStats/Garud.hpp>
+#include <tbb/parallel_for.h>
+#include <tbb/parallel_reduce.h>
 
 using namespace std;
 
@@ -32,9 +35,28 @@ namespace Sequence
   {
     if( d.empty() ) return GarudStats();
     set<string> uhaps(d.begin(),d.end());
-    vector<double> hapcounts;
+	vector<string> vuhaps(std::make_move_iterator(uhaps.begin()),
+			std::make_move_iterator(uhaps.end()));
+    vector<double> hapcounts(uhaps.size());
+	tbb::parallel_for(tbb::blocked_range<std::size_t>(0,uhaps.size()),
+			[&d,&vuhaps,&hapcounts](const tbb::blocked_range<std::size_t> & r) {
+			for(auto i = r.begin();i<r.end();++i)
+			{
+			hapcounts[i] = static_cast<double>(std::count(d.begin(),d.end(),vuhaps[i]));
+			}
+			});
+	const double denom  = static_cast<double>(d.size()*(d.size()-1));
+	double H1 = tbb::parallel_reduce(tbb::blocked_range<double*>(hapcounts.data(),hapcounts.data()+hapcounts.size()),0.,
+			[denom](const tbb::blocked_range<double*> & r,double value) -> double {
+				return std::accumulate(r.begin(),r.end(),value,[denom](double s,double c){
+						return s + c*(c-1.)/denom;
+						});
+				},
+				std::plus<double>()
+			);
     //GarudStats G;
-    double H1 = 0.;
+    /*
+	double H1 = 0.;
     
     for_each(uhaps.cbegin(),uhaps.cend(),
 	     [&](const string & hap) {
@@ -43,6 +65,7 @@ namespace Sequence
 	       H1 += double(hapcount)*double(hapcount-1)/(double(d.size())*double(d.size()-1));
 	       hapcounts.push_back(double(hapcount));
 	     });
+	*/
     sort(hapcounts.begin(),hapcounts.end(),
 	 std::bind(greater<double>(),std::placeholders::_1,std::placeholders::_2));
     double H12 = H1 + 2.*hapcounts[0]*hapcounts[1]/std::pow(double(d.size()),2.);
