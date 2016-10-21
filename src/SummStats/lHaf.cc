e85afff447efee44cd4e85c3860f4147d5b154e8
Kevin R. Thornton <krthornt@uci.edu>
2016-10-21 13:32:01 -0700

    clang-format

--- src/SummStats/lHaf.cc
+++ src/SummStats/lHaf.cc
@@ -15,31 +15,32 @@
  */
 namespace Sequence
 {
-  std::vector<double> lHaf( const SimData & data, const double l )
-  {
-    //Get derived mutation frequency counts per site
-    std::vector<unsigned> dcounts;
-    std::for_each( data.sbegin(), data.send(),
-		   [&dcounts]( const polymorphicSite & p ) {
-		     dcounts.push_back( unsigned(std::count(p.second.begin(),p.second.end(),'1')) );
-		   } );
-    //Get the values for each element in the data
-    std::vector<double> rv(data.size(),0.);
-    for(unsigned i = 0 ; i < data.size() ; ++i )
-      {
-	auto j = std::find_if(data[i].cbegin(),data[i].cend(),[](const char & ch) {
-	    return ch == '1';
-	  });
-	while(j!=data[i].cend())
-	  {
-	    size_t d = size_t(j-data[i].cbegin());
-	    rv[i] += std::pow( double(dcounts[d]),l );
-	    j = std::find_if(j+1,data[i].cend(),
-			     [](const char & ch) {
-			       return ch == '1';
-			     });
-	  }
-      }
-    return rv;
-  }
+    std::vector<double>
+    lHaf(const SimData &data, const double l)
+    {
+        // Get derived mutation frequency counts per site
+        std::vector<unsigned> dcounts;
+        std::for_each(
+            data.sbegin(), data.send(), [&dcounts](const polymorphicSite &p) {
+                dcounts.push_back(unsigned(
+                    std::count(p.second.begin(), p.second.end(), '1')));
+            });
+        // Get the values for each element in the data
+        std::vector<double> rv(data.size(), 0.);
+        for (unsigned i = 0; i < data.size(); ++i)
+            {
+                auto j
+                    = std::find_if(data[i].cbegin(), data[i].cend(),
+                                   [](const char &ch) { return ch == '1'; });
+                while (j != data[i].cend())
+                    {
+                        size_t d = size_t(j - data[i].cbegin());
+                        rv[i] += std::pow(double(dcounts[d]), l);
+                        j = std::find_if(
+                            j + 1, data[i].cend(),
+                            [](const char &ch) { return ch == '1'; });
+                    }
+            }
+        return rv;
+    }
 }
