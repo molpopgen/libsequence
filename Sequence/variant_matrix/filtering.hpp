7e7cc1310c5b29c1e678c597d8f4db2c0ea805f2
molpopgen <krthornt@uci.edu>
2017-10-26 16:06:51 -0700

    add files for filtering a VariantMatrix

---
+++ Sequence/variant_matrix/filtering.hpp
@@ -0,0 +1,18 @@
+#ifndef SEQUENCE_VARIANT_MATRIX_FILTERING_HPP_
+#define SEQUENCE_VARIANT_MATRIX_FILTERING_HPP_
+
+#include <Sequence/VariantMatrix.hpp>
+#include <Sequence/VariantMatrixViews.hpp>
+#include <functional>
+#include <cstdint>
+
+namespace Sequence
+{
+    std::int32_t filter_sites(VariantMatrix &m,
+                              const std::function<bool(const RowView &)> &f);
+    std::int32_t
+    filter_haplotypes(VariantMatrix &m,
+                      const std::function<bool(const ColView &)> &f);
+}
+
+#endif
