82ad5f3fae943b0153f7b853434a9568cb7d79d0
molpopgen <krthornt@uci.edu>
2018-07-12 18:34:38 -0700

    more tests

--- test/testClassicSummstats.cc
+++ test/testClassicSummstats.cc
@@ -4,9 +4,8 @@
 #include <Sequence/VariantMatrixViews.hpp>
 #include <Sequence/summstats/classics.hpp>
 #include <boost/test/unit_test.hpp>
-#include <fstream>
+#include <algorithm>
 #include <iostream>
-#include <unistd.h>
 
 struct dataset
 {
@@ -34,7 +33,7 @@ BOOST_AUTO_TEST_CASE(test_thetapi)
             int ndiffs = 0, ncomps = 0;
             for (std::size_t j = 0; j < m.nsam - 1; ++j)
                 {
-                    for (std::size_t k = j + 1; k < m.nsam ; ++k)
+                    for (std::size_t k = j + 1; k < m.nsam; ++k)
                         {
                             if (r[j] != r[k])
                                 ++ndiffs;
@@ -47,6 +46,51 @@ BOOST_AUTO_TEST_CASE(test_thetapi)
     // Cannot require equal b/c we aren't doing ops
     // in same order.
     BOOST_CHECK_CLOSE(pi, manual, 1e-6);
+}
+
+BOOST_AUTO_TEST_CASE(test_num_poly_sites)
+{
+    auto S = Sequence::nvariable_sites(m);
+    // Not universally true, but is true here:
+    BOOST_REQUIRE_EQUAL(m.nsites, S);
+
+    auto tm = Sequence::total_number_of_mutations(m);
+    // Not universally true, but is true here:
+    BOOST_REQUIRE_EQUAL(m.nsites, tm);
+
+    //Make the last site invariant:
+    auto r = Sequence::get_RowView(m, m.nsites - 1);
+    std::fill(r.begin(), r.end(), 0);
+    S = Sequence::nvariable_sites(m);
+    BOOST_REQUIRE_EQUAL(S, m.nsites - 1);
+    tm = Sequence::total_number_of_mutations(m);
+    BOOST_REQUIRE_EQUAL(m.nsites - 1, tm);
+}
+
+BOOST_AUTO_TEST_CASE(test_total_num_mutations)
+{
+    auto r = Sequence::get_RowView(m, m.nsites - 1);
+    // Add a third character state
+    r[2] = 2;
+    auto tm = Sequence::total_number_of_mutations(m);
+    BOOST_REQUIRE_EQUAL(tm, m.nsites + 1);
+}
+
+BOOST_AUTO_TEST_CASE(test_thetaw)
+// The above two tests imply that
+// thetaw is correct.  Thus,
+// all we're doing below is
+// checking the denominator.
+{
+    auto w = Sequence::thetaw(m);
+    double S = m.nsites;
+    double d = 0.0;
+    for (int i = 1; i < m.nsam; ++i)
+        {
+            d += 1. / static_cast<double>(i);
+        }
+    auto manual = S / d;
+    BOOST_CHECK_CLOSE(w, manual, 1e-6);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
