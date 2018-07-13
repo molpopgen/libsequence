7e4221141eef8549130931e224976c5d4544d2ed
molpopgen <krthornt@uci.edu>
2018-07-13 09:31:57 -0700

    Refine StateCounts and process_variable_sites:
    1. No more default parameter values.  They simply lead to confusion.
    2. More overloads replace what defaults did.

--- src/variant_matrix/StateCounts.cc
+++ src/variant_matrix/StateCounts.cc
@@ -1,45 +1,60 @@
 #include <Sequence/StateCounts.hpp>
 #include <stdexcept>
 
+inline static decltype(Sequence::StateCounts::n)
+fill_counts(decltype(Sequence::StateCounts::counts)& counts,
+            const Sequence::ConstRowView& r)
+{
+
+    decltype(Sequence::StateCounts::n) n{ 0u };
+    for (auto i : r)
+        {
+            if (i == Sequence::VariantMatrix::mask)
+                {
+                    throw std::invalid_argument("reserved value encountered");
+                }
+            if (i < 0)
+                i = -1;
+            auto itr = counts.find(i);
+            if (itr == counts.end())
+                {
+                    counts.insert(std::make_pair(i, 1));
+                }
+            else
+                {
+                    itr->second++;
+                }
+        }
+    for (const auto& ci : counts)
+        {
+            if (ci.first >= 0)
+                {
+                    n += ci.second;
+                }
+        }
+    return n;
+}
+
 namespace Sequence
 {
+    StateCounts::StateCounts(const ConstRowView& r)
+        : counts{}, n{ 0u }, refstate(-1)
+    {
+        n = fill_counts(counts, r);
+    }
+
     StateCounts::StateCounts(const ConstRowView& r,
                              const std::int8_t refstate_)
         : counts{}, n{ 0u }, refstate(refstate_)
     {
-        for (auto i : r)
-            {
-                if (i == VariantMatrix::mask)
-                    {
-                        throw std::invalid_argument(
-                            "reserved value encountered");
-                    }
-                if (i < 0)
-                    i = -1;
-                auto itr = counts.find(i);
-                if (itr == counts.end())
-                    {
-                        counts.insert(std::make_pair(i, 1));
-                    }
-                else
-                    {
-                        itr->second++;
-                    }
-            }
-        for(const auto & ci : counts)
-        {
-            if(ci.first >= 0)
-            {
-                n += ci.second;
-            }
-        }
+        n = fill_counts(counts, r);
     }
 
     std::vector<StateCounts>
     process_variable_sites(const VariantMatrix& m,
                            const std::vector<std::int8_t>& refstates)
     {
-        if (!refstates.empty() && refstates.size() != m.nsites)
+        if (refstates.size() != m.nsites)
             {
                 throw std::invalid_argument("refstates.size() != m.nsites");
             }
@@ -47,15 +62,26 @@ namespace Sequence
         rv.reserve(m.nsites);
         for (std::size_t i = 0; i < m.nsites; ++i)
             {
-                if (refstates.empty())
-                    {
-                        rv.emplace_back(get_ConstRowView(m, i));
-                    }
-                else
-                    {
-                        rv.emplace_back(get_ConstRowView(m, i), refstates[i]);
-                    }
+                rv.emplace_back(get_ConstRowView(m, i), refstates[i]);
             }
         return rv;
     }
-}
+
+    std::vector<StateCounts>
+    process_variable_sites(const VariantMatrix& m, const std::int8_t refstate)
+    {
+        std::vector<StateCounts> rv;
+        rv.reserve(m.nsites);
+        for (std::size_t i = 0; i < m.nsites; ++i)
+            {
+                rv.emplace_back(get_ConstRowView(m, i), refstate);
+            }
+        return rv;
+    }
+
+    std::vector<StateCounts>
+    process_variable_sites(const VariantMatrix& m)
+    {
+        return process_variable_sites(m, -1);
+    }
+} // namespace Sequence
