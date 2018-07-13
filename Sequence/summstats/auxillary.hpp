856ef3a0cdc8540569a918332b8766695150492b
molpopgen <krthornt@uci.edu>
2018-07-13 13:40:31 -0700

    Add b_sub_n_plus1 from Zeng et al.

--- Sequence/summstats/auxillary.hpp
+++ Sequence/summstats/auxillary.hpp
@@ -9,6 +9,7 @@ namespace Sequence
     {
         double a_sub_n(const std::uint32_t);
         double b_sub_n(const std::uint32_t nsam);
+        double b_sub_n_plus1(const std::uint32_t nsam);
     } // namespace summstats_aux
 } // namespace Sequence
 
