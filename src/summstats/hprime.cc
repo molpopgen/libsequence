6e0554b3fd6d18d3cfdd15c9c2bda989d34d5ebb
molpopgen <krthornt@uci.edu>
2018-07-13 15:41:15 -0700

    add hprime.cc to build system

--- src/summstats/hprime.cc
+++ src/summstats/hprime.cc
@@ -2,7 +2,7 @@
 #include <Sequence/VariantMatrixViews.hpp>
 #include <Sequence/summstats/thetapi.hpp>
 #include <Sequence/summstats/thetal.hpp>
-#include <Sequence/summstats/allele_counts.hpp>
+#include <Sequence/summstats/nvariablesites.hpp>
 #include <Sequence/summstats/auxillary.hpp>
 
 namespace Sequence
@@ -10,14 +10,20 @@ namespace Sequence
     double
     hprime(const VariantMatrix &m, const std::int8_t refstate)
     {
-        auto tp = thetapi(m, refstate);
+        auto tp = thetapi(m);
         auto tl = thetal(m, refstate);
+        if (tp == 0.0)
+            {
+                return std::numeric_limits<double>::quiet_NaN();
+            }
 
-        auto a = summstats_aux::a_sub_n(m.nsam);
-        auto b = summstats_aux::b_sub_n(m.nsam);
-        auto b1 = summstats_aux::b_sub_n_plus1(m.nsam);
+        auto a = summstats_aux::a_sub_n(static_cast<std::uint32_t>(m.nsam));
+        auto b = summstats_aux::b_sub_n(static_cast<std::uint32_t>(m.nsam));
+        auto b1
+            = summstats_aux::b_sub_n_plus1(static_cast<std::uint32_t>(m.nsam));
 
         //Count number of bi-allelic sites
         //TODO: generalize this
+        auto S = nbiallelic_sites(m);
     }
 } // namespace Sequence
