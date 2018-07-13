b7ececafaf7e6f557186cdeb9111b4b2c81a984e
molpopgen <krthornt@uci.edu>
2018-07-13 15:48:49 -0700

    Implement H'.

--- src/summstats/hprime.cc
+++ src/summstats/hprime.cc
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <Sequence/VariantMatrix.hpp>
 #include <Sequence/VariantMatrixViews.hpp>
 #include <Sequence/summstats/thetapi.hpp>
@@ -25,5 +26,21 @@ namespace Sequence
         //Count number of bi-allelic sites
         //TODO: generalize this
         auto S = nbiallelic_sites(m);
+        double tw
+            = static_cast<double>(S) / a; //TODO: replace with call to thetaw
+        double tsq = S * (S - 1) / (a * a + b);
+        double n = static_cast<double>(m.nsam);
+
+        double vThetal
+            = (n * tw) / (2.0 * (n - 1.0))
+              + (2.0 * std::pow(n / (n - 1.0), 2.0) * (b1 - 1.0) - 1.0) * tsq;
+        double vPi = (3.0 * n * (n + 1.0) * tw + 2.0 * (n * n + n + 3.0) * tsq)
+                     / (9 * n * (n - 1.0));
+        double cov
+            = ((n + 1.0) / (3.0 * (n - 1.0))) * tw
+              + ((7.0 * n * n + 3.0 * n - 2.0 - 4.0 * n * (n + 1.0) * b1)
+                 / (2.0 * std::pow((n - 1.0), 2.0)))
+                    * tsq;
+        return (tp - tl) / std::pow(vThetal + vPi - 2.0 * cov, 0.5);
     }
 } // namespace Sequence
