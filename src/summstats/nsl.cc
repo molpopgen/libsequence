ad82410c3eb6c5dc6a469d38cdf9016db3aa35e8
molpopgen <krthornt@uci.edu>
2018-07-20 15:51:59 -0700

    Fix errors in nSL implementation found during testing via example.

--- src/summstats/nsl.cc
+++ src/summstats/nsl.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <algorithm>
 #include <cmath>
 #include <cstdint>
 #include <Sequence/VariantMatrix.hpp>
@@ -28,7 +30,7 @@ namespace
     std::int64_t
     get_right(const Sequence::ConstColView& sample_i,
               const Sequence::ConstColView& sample_j, const std::size_t core,
-              const std::int64_t nsam)
+              const std::int64_t nsites)
     {
         std::int64_t right = static_cast<std::int64_t>(core) + 1;
         std::size_t right_index;
@@ -41,17 +43,18 @@ namespace
                         break;
                     }
             }
-        while (++right < nsam);
+        while (++right < nsites);
         return right;
     }
 
     void
     update_counts(double nsl_values[2], double ihs_values[2], int counts[2],
-                  const std::size_t nsam, const std::vector<double>& positions,
+                  const std::size_t nsites,
+                  const std::vector<double>& positions,
                   const std::size_t index, const std::int64_t left,
                   const std::int64_t right)
     {
-        if (left >= 0 && static_cast<std::size_t>(right) < nsam)
+        if (left >= 0 && static_cast<std::size_t>(right) < nsites)
             //Then there are SNPs differentiating
             //i and j within the region
             {
@@ -83,26 +86,32 @@ namespace Sequence
                 auto sample_i = get_ConstColView(m, i);
                 for (std::size_t j = i + 1; j < m.nsam; ++j)
                     {
-                        auto sample_j = get_ConstColView(m, j);
-                        //Find where samples i and j differ
-                        auto left = get_left(sample_i, sample_j, core);
-                        auto right
-                            = get_right(sample_i, sample_j, core,
-                                        static_cast<std::int64_t>(m.nsam));
-                        update_counts(
-                            nsl_values, ihs_values, counts, m.nsam,
-                            m.positions,
-                            static_cast<std::size_t>(core_view[i] == refstate),
-                            left, right);
+                        if (core_view[i] == core_view[j])
+                            {
+                                auto sample_j = get_ConstColView(m, j);
+                                //Find where samples i and j differ
+                                auto left = get_left(sample_i, sample_j, core);
+                                auto right = get_right(
+                                    sample_i, sample_j, core,
+                                    static_cast<std::int64_t>(m.nsites));
+                                update_counts(nsl_values, ihs_values, counts,
+                                              m.nsites, m.positions,
+                                              static_cast<std::size_t>(
+                                                  core_view[i] == refstate),
+                                              left, right);
+                            }
                     }
             }
-        double nSL_num = nsl_values[0] / static_cast<double>(counts[0]);
-        double nSL_den = nsl_values[1] / static_cast<double>(counts[1]);
-        double iHS_num = nsl_values[0] / static_cast<double>(counts[0]);
-        double iHS_den = nsl_values[1] / static_cast<double>(counts[1]);
-        return nSL{ //TODO fix core count
-                    std::log(nSL_num) - std::log(nSL_den),
-                    std::log(iHS_num) - std::log(iHS_den), 1
-        };
+        double nSL_den = nsl_values[0] / static_cast<double>(counts[0]);
+        double nSL_num = nsl_values[1] / static_cast<double>(counts[1]);
+        double iHS_den = nsl_values[0] / static_cast<double>(counts[0]);
+        double iHS_num = nsl_values[1] / static_cast<double>(counts[1]);
+        auto nonrefcount = static_cast<std::int32_t>(
+            std::count_if(core_view.begin(), core_view.end(),
+                          [refstate](const std::int8_t i) {
+                              return i >= 0 && i != refstate;
+                          }));
+        return nSL{ std::log(nSL_num) - std::log(nSL_den),
+                    std::log(iHS_num) - std::log(iHS_den), nonrefcount };
     }
 } // namespace Sequence
