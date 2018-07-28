3ba0844b9fd7847ac150db846294ca23ce10e16c
molpopgen <krthornt@uci.edu>
2018-07-28 14:43:34 -0700

    Draft implementations of H1, H12, H2/H1.

--- src/summstats/garud.cc
+++ src/summstats/garud.cc
@@ -1,4 +1,12 @@
+#include <cstdint>
+#include <limits>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
 #include <Sequence/summstats/garud.hpp>
+#include <Sequence/summstats/generic.hpp>
+#include <Sequence/VariantMatrix.hpp>
+#include <Sequence/summstats/classics.hpp>
 
 namespace Sequence
 {
@@ -12,5 +20,54 @@ namespace Sequence
                            const double __h2h1)
         : H1(__h1), H12(__h12), H2H1(__h2h1)
     {
+    }
+
+    GarudStats
+    garud_statistics(const VariantMatrix& m)
+    {
+        GarudStats rv;
+        if (m.data.empty() || !m.nsam)
+            {
+                return rv;
+            }
+        // Although one of the stats is haplotype diversity,
+        // w
+        auto labels = label_haplotypes(m);
+        std::unordered_map<std::int32_t, std::int32_t> counts;
+        std::size_t nmissing = 0;
+        for (auto l : labels)
+            {
+                if (l < 0)
+                    {
+                        ++nmissing;
+                    }
+                else
+                    {
+                        counts[l]++;
+                    }
+            }
+        if (counts.size() < 2)
+            {
+                return rv;
+            }
+        rv.H1 = 1.0 - diversity_from_counts(counts, m.nsam - nmissing);
+        std::vector<std::pair<std::int32_t, std::int32_t>> vcounts(
+            counts.begin(), counts.end());
+        std::sort(vcounts.begin(), vcounts.end(),
+                  [](decltype(vcounts[0])& a, decltype(vcounts[0])& b) {
+                      return a.second < b.second;
+                  });
+        double nsam
+            = static_cast<double>(m.nsam) - static_cast<double>(nmissing);
+        rv.H12 = rv.H1
+                 + 2. * static_cast<double>(vcounts[0].second)
+                       * static_cast<double>(vcounts[1].second)
+                       / (nsam * (nsam - 1.0));
+        rv.H2H1 = (rv.H1
+                   - static_cast<double>(vcounts[0].second
+                                         * (vcounts[0].second - 1))
+                         / (nsam * (nsam - 1)))
+                  / rv.H1;
+        return rv;
     }
 } // namespace Sequence
