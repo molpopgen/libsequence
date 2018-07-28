1e7a9f496f949f4bbd469531982efc20ba40bfa3
molpopgen <krthornt@uci.edu>
2018-07-28 14:47:16 -0700

    add source files for Garud stat unit tests

---
+++ test/testGarudStatistics.cc
@@ -0,0 +1,17 @@
+//! \file testGarudStatistics.cc @brief unit tests for H1, H12, H2/H1 stats
+
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <Sequence/VariantMatrix.hpp>
+#include <Sequence/VariantMatrixViews.hpp>
+#include <Sequence/summstats/garud.hpp>
+#include "VariantMatrixFixture.hpp"
+#include <boost/test/unit_test.hpp>
+
+BOOST_FIXTURE_TEST_SUITE(test_garud_stats, dataset)
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
