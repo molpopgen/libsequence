d0ea99dd2cdd5a6016cd5cac215fc34dfa679e99
molpopgen <krthornt@uci.edu>
2018-08-05 14:37:39 -0700

    docs for aggregate_sites

--- Sequence/summstats/algorithm.hpp
+++ Sequence/summstats/algorithm.hpp
@@ -1,3 +1,6 @@
+/*!
+\file summstats/algorithm.hpp
+*/
 #ifndef SEQUENCE_SUMMSTATS_ALGORITHM_HPP
 #define SEQUENCE_SUMMSTATS_ALGORITHM_HPP
 
@@ -14,6 +17,30 @@ namespace Sequence
         inline void
         aggregate_sites(const VariantMatrix& m, const F& f,
                         const std::int8_t refstate)
+        /*! Helper algorithm for implementing summary statistics.
+         *
+         *  Several common summary statistics are combinations of
+         *  others.  Examples include Tajima's D, Fay and Wu's H,
+         *  etc..  If we take D as an example, it is tempting to
+         *  use existing functions, such as Sequence::thetapi and
+         *  Sequence::thetaw, as intermediate steps.
+         *  However, doing so goes over the data multiple times.  
+         *
+         *  Fortunately, these statistics are often easy enough to 
+         *  implement that we could calculate pi and Watterson's theta 
+         *  in one loop.  This function helps you do that.
+         *  
+         *  \param m A VariantMatrix
+         *  \param f A function taking a const StateCounts & and returning nothing.
+         *  \param refstate The reference state.
+         *
+         *  This function loops over \a m.nsites and passes the state counts on
+         *  to the aggregator function \a f.
+         *
+         *  See the implementation of Sequence::tajd for an example.
+         *
+         *  \ingroup popgenanalysis
+         */
         {
             StateCounts c(refstate);
             for (std::size_t i = 0; i < m.nsites; ++i)
