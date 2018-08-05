74614d55e8cf71e5c127942cbc44e8347a5bcb67
molpopgen <krthornt@uci.edu>
2018-08-05 13:07:49 -0700

    Refactor Sequence::tajd to go over data only once.

--- src/summstats/tajd.cc
+++ src/summstats/tajd.cc
@@ -1,22 +1,43 @@
 #include <cmath>
 #include <limits>
-#include <Sequence/summstats/thetapi.hpp>
-#include <Sequence/summstats/thetaw.hpp>
-#include <Sequence/summstats/nvariablesites.hpp>
+#include <Sequence/summstats/algorithm.hpp>
+#include <Sequence/StateCounts.hpp>
+#include <Sequence/VariantMatrixViews.hpp>
 #include <Sequence/summstats/auxillary.hpp>
+
 namespace Sequence
 {
     double
     tajd(const VariantMatrix& m)
     {
-        auto S = total_number_of_mutations(m);
-        if (S == 0)
+        unsigned S = 0;
+        double pi = 0.0;
+        const auto agg = [&S, &pi](const StateCounts& c) {
+            unsigned nstates = 0;
+            double homozygosity = 0.0;
+            for (auto i : c.counts)
+                {
+                    if (i)
+                        {
+                            nstates++;
+                            homozygosity += static_cast<double>(i)
+                                            * static_cast<double>(i - 1);
+                        }
+                }
+            if (nstates > 1)
+                {
+                    S += nstates - 1;
+                    double d = static_cast<double>(c.n * (c.n - 1));
+                    pi += (1.0 - homozygosity / d);
+                }
+        };
+        sstats_algo::aggregate_sites(m, agg, -1);
+        if (!S)
             {
                 return std::numeric_limits<double>::quiet_NaN();
             }
-        auto w = thetaw(m);
-        auto pi = thetapi(m);
         auto a1 = summstats_aux::a_sub_n(static_cast<std::uint32_t>(m.nsam));
+        double w = static_cast<double>(S) / a1;
         auto a2 = summstats_aux::b_sub_n(static_cast<std::uint32_t>(m.nsam));
         auto dn = static_cast<double>(m.nsam);
         double b1 = (dn + 1.0) / (3.0 * (dn - 1.0));
