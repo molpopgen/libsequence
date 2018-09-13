47ada1d531f702779655e8f736cfcd42fb1ff919
molpopgen <krthornt@uci.edu>
2018-09-13 14:21:52 -0700

    add docs for make_slice

--- Sequence/variant_matrix/windows.hpp
+++ Sequence/variant_matrix/windows.hpp
@@ -18,7 +18,18 @@ namespace Sequence
      */
     VariantMatrix make_window(const VariantMatrix& m, const double beg,
                               const double end);
-
+    /*! \brief Return a slice from a VariantMatrix
+     * \param m A VariantMatrix
+     * \param beg Beginning of window
+     * \param end End of window
+     * \param i index of first haplotype to include
+     * \param j one past last haplotype to include
+     *
+     * The result is a variant matrix including positions [beg,end]
+     * and samples [i,j) from \a m.  Note that the sample interval is 
+     * half-open!
+     */
+    
     VariantMatrix make_slice(const VariantMatrix& m, const double beg,
                              const double end,
                              const std::size_t i,
