254e10cf67b0b00eae736fba1f1948bf7ae235d9
molpopgen <krthornt@uci.edu>
2018-09-13 10:16:58 -0700

    Add source files for taking slices from a VariantMatrix

---
+++ Sequence/variant_matrix/windows.hpp
@@ -0,0 +1,24 @@
+#ifndef SEQUENCE_VARIANT_MATRIX_WINDOWS_HPP
+#define SEQUENCE_VARIANT_MATRIX_WINDOWS_HPP
+
+#include <algorithm>
+#include <vector>
+#include <stdexcept>
+#include <Sequence/VariantMatrix.hpp>
+#include <Sequence/VariantMatrixViews.hpp>
+
+namespace Sequence
+{
+    /*! \brief Return a window from a VariantMatrix
+     * \param m A VariantMatrix
+     * \param beg Beginning of window
+     * \param end End of window
+     *
+     * \note The window intervals are half-open, [beg,end)
+     */
+    VariantMatrix make_window(const VariantMatrix& m, const double beg,
+                              const double end);
+
+} // namespace Sequence
+
+#endif
---
+++ src/variant_matrix/windows.cc
@@ -0,0 +1,33 @@
+#include <Sequence/variant_matrix/windows.hpp>
+
+namespace Sequence
+{
+    VariantMatrix
+    make_window(const VariantMatrix& m, const double beg, const double end)
+    {
+        if (end < beg)
+            {
+                throw std::invalid_argument("end must be >= beg");
+            }
+        auto pb
+            = std::lower_bound(m.positions.begin(), m.positions.end(), beg);
+        auto pe = std::lower_bound(pb, m.positions.end(), end);
+        decltype(m.data) data;
+        decltype(m.positions) pos;
+        if (pb == m.positions.end())
+            {
+                return VariantMatrix(std::move(data), std::move(pos));
+            }
+
+        pos.assign(pb, pe);
+        for (auto i = pb; i < pe; ++i)
+            {
+                auto v = get_ConstRowView(
+                    m, static_cast<std::size_t>(
+                           std::distance(m.positions.begin(), i)));
+                data.insert(data.end(),v.begin(),v.end());
+            }
+        return VariantMatrix(std::move(data), std::move(pos));
+    }
+
+} // namespace Sequence
