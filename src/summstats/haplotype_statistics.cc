c7f91272861a099b7b5bb9e1e034904d30a5a640
molpopgen <krthornt@uci.edu>
2018-09-19 13:33:53 -0700

    Update implementation of is_different_matrix: 1. pre-compute column views 2. Use mismatch_skip_missing to determine differences.

--- src/summstats/haplotype_statistics.cc
+++ src/summstats/haplotype_statistics.cc
@@ -6,6 +6,7 @@
 #include <Sequence/summstats/generic.hpp>
 #include <Sequence/VariantMatrix.hpp>
 #include <Sequence/VariantMatrixViews.hpp>
+#include "algorithm.hpp"
 
 namespace Sequence
 {
@@ -46,26 +47,20 @@ namespace Sequence
     {
         std::vector<std::int32_t> rv;
         rv.reserve(m.nsam);
+        std::vector<ConstColView> alleles;
+        alleles.reserve(m.nsam);
+        for (std::size_t i = 0; i < m.nsam; ++i)
+            {
+                alleles.push_back(get_ConstColView(m, i));
+            }
         for (std::size_t i = 0; i < m.nsam - 1; ++i)
             {
-                auto ci = get_ConstColView(m, i);
                 for (std::size_t j = i + 1; j < m.nsam; ++j)
                     {
-                        auto cj = get_ConstColView(m, j);
-                        auto cib = ci.begin(), cjb = cj.begin();
-                        std::int32_t different = 0;
-                        //If both sites are not missing
-                        //and not equal, they are different
-                        while (cib != ci.end() && !different)
-                            {
-                                if (*cib >= 0 && *cjb >= 0 && *cib != *cjb)
-                                    {
-                                        different = 1;
-                                    }
-                                ++cib;
-                                ++cjb;
-                            }
-                        rv.push_back(different);
+                        auto m = summstats_algo::mismatch_skip_missing(
+                            alleles[i].begin(), alleles[i].end(),
+                            alleles[j].begin());
+                        rv.push_back(m.first != alleles[i].end());
                     }
             }
         return rv;
