5817a4f3f2921c2f5e92d2e26d9037218af92711
molpopgen <krthornt@uci.edu>
2018-09-19 12:50:11 -0700

    Add new algorithm header for summary stats. Implement mismatch_skip_missing.

---
+++ src/summstats/algorithm.hpp
@@ -0,0 +1,25 @@
+#ifndef SEQUENCE_SUMMSTATS_ALGORITHM
+#define SEQUENCE_SUMMSTATS_ALGORITHM
+
+#include <utility>
+#include <algorithm>
+
+namespace Sequence
+{
+    namespace summstats_algo
+    {
+        template <typename iterator>
+        inline std::pair<iterator, iterator>
+        mismatch_skip_missing(iterator beg, iterator end, iterator beg2)
+        {
+            auto m = std::mismatch(beg, end, beg2);
+            while (m.first < end && (*m.first < 0 || *m.second < 0))
+                {
+                    m = std::mismatch(beg + 1, end, beg2 + 1);
+                }
+            return m;
+        }
+    } // namespace summstats_algo
+} // namespace Sequence
+
+#endif
