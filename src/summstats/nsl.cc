e63a5ee330b88ba49034990eb39e5b529a8a6463
molpopgen <krthornt@uci.edu>
2018-09-18 18:31:15 -0700

    fix error in get_right for case of missing data

--- src/summstats/nsl.cc
+++ src/summstats/nsl.cc
@@ -41,7 +41,7 @@ namespace
         while (m.first < sample_i.end() && (*m.first < 0 || *m.second < 0))
             // Do not let missing data break homozygosity runs
             {
-                m = std::mismatch(m.first, sample_i.end(), m.second);
+                m = std::mismatch(m.first + 1, sample_i.end(), m.second + 1);
             }
         return std::distance(sample_i.begin(), m.first);
     }
@@ -66,7 +66,8 @@ namespace
                 std::int64_t left_edge = edges[lindex];
                 if (left_edge == -1)
                     {
-                        if (hapi[0] != hapj[0] && !(hapi[0]<0||hapj[0]<0))
+                        if (hapi[0] != hapj[0]
+                            && !(hapi[0] < 0 || hapj[0] < 0))
                             {
                                 left_edge = 0;
                                 edges[lindex] = left_edge;
@@ -86,7 +87,7 @@ namespace
                             }
                     }
             }
-        else if (!(core_view[i]<0||core_view[j]<0))
+        else if (!(core_view[i] < 0 || core_view[j] < 0))
             {
                 edges[lindex] = static_cast<std::int64_t>(core);
             }
