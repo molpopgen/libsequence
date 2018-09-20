8662ff97ecc195e71f811470d65a93574c82e96c
molpopgen <krthornt@uci.edu>
2018-09-20 09:57:06 -0700

    This is the first version of what I believe to be a working implementation of nSL(x).

--- src/summstats/nslx.cc
+++ src/summstats/nslx.cc
@@ -1,7 +1,6 @@
 #include <algorithm>
 #include <Sequence/summstats/nsl.hpp>
 #include <Sequence/VariantMatrixViews.hpp>
-#include <iostream>
 #include "nsl_common.hpp"
 
 namespace
@@ -9,7 +8,6 @@ namespace
     inline bool
     update_edge_matrix(const Sequence::VariantMatrix& m,
                        const std::vector<std::int64_t>& xtons,
-                       const std::pair<std::int64_t, std::int64_t> flanks,
                        const Sequence::ConstRowView& core_view,
                        const Sequence::ConstColView& hapi,
                        const Sequence::ConstColView& hapj,
@@ -22,86 +20,48 @@ namespace
         bool rv = false;
         if (core_view[i] == core_view[j] && !(core_view[i] < 0))
             {
-                if (edges.left == -1)
-                    {
-                        //Variant 0 can only break homozygosity
-                        //run if it is an xton
-                        if (xtons.front() == 0 && hapi[0] != hapj[0]
-                            && !(hapi[0] < 0 || hapj[0] < 0))
-                            {
-                                edges.left = 0;
-                                rv = true;
-                            }
-                    }
+                rv = true;
                 if (edges.left >= 0)
                     {
                         if (edges.right == -1
                             || static_cast<std::size_t>(edges.right) <= core)
                             {
-                                rv = false;
+                                edges.right = m.nsites;
                                 // To update right edge:
                                 // Iterate over all xtons > core
                                 // and check if haplotypes i,j
                                 // differ at those sites.
-                                for (auto r = flanks.second;
-                                     r < xtons.size() && rv == false; ++r)
+                                auto xton_gt_core = std::upper_bound(
+                                    xtons.begin(), xtons.end(), core);
+                                for (; xton_gt_core < xtons.end();
+                                     ++xton_gt_core)
                                     {
-                                        if (!(core <= xtons[r]))
+                                        if (hapi[*xton_gt_core]
+                                                != hapj[*xton_gt_core]
+                                            && !(hapi[*xton_gt_core] < 0
+                                                 || hapj[*xton_gt_core] < 0))
                                             {
-                                                throw std::logic_error(
-                                                    "r < core");
-                                            }
-                                        if (hapi[xtons[r]] != hapj[xtons[r]]
-                                            && !(hapi[xtons[r]] < 0
-                                                 || hapj[xtons[r]] < 0))
-                                            {
-                                                rv = true;
-                                                edges.right = r;
+                                                edges.right = *xton_gt_core;
+                                                break;
                                             }
                                     }
                             }
                     }
             }
         else if (!(hapi[core] < 0 || hapj[core] < 0)
-                 && std::binary_search(xtons.begin() + flanks.first,
-                                       xtons.begin() + flanks.second, core))
+                 && std::binary_search(xtons.begin(), xtons.end(), core))
             {
                 //seqs i and j differ and core is an xton,
                 //thus core is a new left
                 edges.left = core;
             }
+
         return rv;
-    }
-    std::pair<std::int64_t, std::int64_t>
-    get_flanking_xtons(const std::vector<int64_t>& xtons,
-                       const std::size_t core)
-    {
-        auto left_xton = std::upper_bound(
-            xtons.rbegin(), xtons.rend(), core,
-            [](const std::int64_t element, const std::size_t value) {
-                return element > value;
-            });
-        if (left_xton == xtons.rend())
-            {
-                return std::make_pair(-1, -1);
-            }
-        auto right_xton = std::upper_bound(
-            left_xton.base(), xtons.end(), core,
-            [](const std::int64_t element, const std::size_t value) {
-                return element < value;
-            });
-        if (right_xton == xtons.end())
-            {
-                return std::make_pair(-1, -1);
-            }
-        return std::make_pair(std::distance(xtons.begin(), left_xton.base()),
-                              std::distance(xtons.begin(), right_xton));
     }
 } // namespace
 
 namespace Sequence
 {
-    //TODO refactor to use suffix_edges
     std::vector<nSLiHS>
     nslx(const VariantMatrix& m, const std::int8_t refstate, const int x)
     {
@@ -116,7 +76,7 @@ namespace Sequence
                     r.begin(), r.end(), [refstate](const std::int8_t a) {
                         return a != refstate && !(a < 0);
                     });
-                if (nonref == x)
+                if (nonref > 0 && nonref <= x)
                     {
                         xtons.push_back(i);
                     }
@@ -140,37 +100,27 @@ namespace Sequence
                 auto core_view = get_ConstRowView(m, core);
                 // Doing any work requires the existence
                 // of x-tons left and right of core
-                auto flanks = get_flanking_xtons(xtons, core);
                 double nsl_values[2] = { 0, 0 };
                 double ihs_values[2] = { 0, 0 };
                 int counts[2] = { 0, 0 };
                 std::size_t pair_index = 0;
-                if (flanks.first != -1)
+                for (std::size_t i = 0; i < m.nsam - 1; ++i)
                     {
-                        for (std::size_t i = 0; i < m.nsam - 1; ++i)
+                        for (std::size_t j = i + 1; j < m.nsam;
+                             ++j, ++pair_index)
                             {
-                                for (std::size_t j = i + 1; j < m.nsam;
-                                     ++j, ++pair_index)
+                                if (update_edge_matrix(m, xtons, core_view,
+                                                       alleles[i], alleles[j],
+                                                       edges[pair_index], core,
+                                                       i, j))
                                     {
-                                        if (update_edge_matrix(
-                                                m, xtons, flanks, core_view,
-                                                alleles[i], alleles[j],
-                                                edges[pair_index],
-                                                core, i, j))
-                                            {
-                                                summstats_details::
-                                                    update_counts(
-                                                        nsl_values, ihs_values,
-                                                        counts, m.nsites,
-                                                        m.positions,
-                                                        static_cast<
-                                                            std::size_t>(
-                                                            core_view[i]
-                                                            == refstate),
-                                                        edges[pair_index].left,
-                                                        edges[pair_index]
-                                                            .right);
-                                            }
+                                        summstats_details::update_counts(
+                                            nsl_values, ihs_values, counts,
+                                            m.nsites, m.positions,
+                                            static_cast<std::size_t>(
+                                                core_view[i] == refstate),
+                                            edges[pair_index].left,
+                                            edges[pair_index].right);
                                     }
                             }
                     }
