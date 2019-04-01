b40463cef670c14959845c10c9081aee2cd75a2b
molpopgen <krthornt@uci.edu>
2019-04-01 08:40:48 -0700

    rename header file for deprecated summstats

---
+++ Sequence/SummStatsDeprecated.hpp
@@ -0,0 +1,13 @@
+#ifndef __SEQUENCE_SUMMSTATS_HPP__
+#define __SEQUENCE_SUMMSTATS_HPP__
+
+/*! \file SummStatsDeprecated.hpp
+  Header file for summary statistic of variation data.
+*/
+
+#include <Sequence/SummStatsDeprecated/nSL.hpp>
+#include <Sequence/SummStatsDeprecated/Garud.hpp>
+#include <Sequence/SummStatsDeprecated/lHaf.hpp>
+#include <Sequence/SummStatsDeprecated/Snn.hpp>
+
+#endif
