ea10d6a38391c63046c5bd7f238ee6d781254de7
molpopgen <krthornt@uci.edu>
2019-06-14 05:37:43 -0700

    Add implementation file.

---
+++ src/variant_matrix/nonowningcapsules.cc
@@ -0,0 +1,225 @@
+#include <Sequence/NonOwningCapsules.hpp>
+#include <limits>
+#include <algorithm>
+#include <iterator>
+#include <exception>
+
+namespace
+{
+    void
+    raise()
+    {
+        throw std::runtime_error("data are read-only");
+    }
+} // namespace
+
+namespace Sequence
+{
+    std::size_t&
+    NonOwningGenotypeCapsule::nsites()
+    {
+        return nsites_;
+    }
+
+    std::size_t&
+    NonOwningGenotypeCapsule::nsam()
+    {
+        return nsam_;
+    }
+
+    std::size_t
+    NonOwningGenotypeCapsule::nsites() const
+    {
+        return nsites_;
+    }
+
+    std::size_t
+    NonOwningGenotypeCapsule::nsam() const
+    {
+        return nsam_;
+    }
+
+    std::int8_t& NonOwningGenotypeCapsule::operator()(std::size_t, std::size_t)
+    {
+        raise();
+    }
+
+    const std::int8_t& NonOwningGenotypeCapsule::
+    operator()(std::size_t site, std::size_t sample) const
+    {
+        return buffer[0]; // TODO: fix
+    }
+
+    std::int8_t*
+    NonOwningGenotypeCapsule::data()
+    {
+        raise();
+    }
+
+    const std::int8_t*
+    NonOwningGenotypeCapsule::data() const
+    {
+        return buffer.get();
+    }
+
+    const std::int8_t*
+    NonOwningGenotypeCapsule::cdata() const
+    {
+        return buffer.get();
+    }
+
+    std::unique_ptr<GenotypeCapsule>
+    NonOwningGenotypeCapsule::clone() const
+    {
+        return std::unique_ptr<GenotypeCapsule>(new NonOwningGenotypeCapsule(
+            this->buffer.get(), this->nsites_, this->nsam_, this->tda_));
+    }
+
+    std::int8_t*
+    NonOwningGenotypeCapsule::begin()
+    {
+        raise();
+    }
+
+    const std::int8_t*
+    NonOwningGenotypeCapsule::begin() const
+    {
+        return buffer.get();
+    }
+
+    std::int8_t*
+    NonOwningGenotypeCapsule::end()
+    {
+        raise();
+    }
+
+    const std::int8_t*
+    NonOwningGenotypeCapsule::end() const
+    {
+        return buffer.get() + nsam_ * nsites_;
+    }
+
+    const std::int8_t*
+    NonOwningGenotypeCapsule::cbegin() const
+    {
+        return cdata();
+    }
+
+    const std::int8_t*
+    NonOwningGenotypeCapsule::cend() const
+    {
+        return cdata() + nsam_ + nsites_;
+    }
+
+    bool
+    NonOwningGenotypeCapsule::empty() const
+    {
+        return nsam_ == 0 || nsites_ == 0;
+    }
+
+    std::size_t
+    NonOwningGenotypeCapsule::size() const
+    {
+        return nsam_ * nsites_;
+    }
+
+    bool
+    NonOwningGenotypeCapsule::resizable() const
+    {
+        return false;
+    }
+
+    // Position capsule based on std::vector here
+    double& NonOwningPositionCapsule::operator[](std::size_t i) { raise(); }
+
+    const double& NonOwningPositionCapsule::operator[](std::size_t i) const
+    {
+        return buffer[i];
+    }
+
+    double*
+    NonOwningPositionCapsule::data()
+    {
+        raise();
+    }
+
+    const double*
+    NonOwningPositionCapsule::data() const
+    {
+        return buffer.get();
+    }
+
+    const double*
+    NonOwningPositionCapsule::cdata() const
+    {
+        return buffer.get();
+    }
+
+    std::unique_ptr<PositionCapsule>
+    NonOwningPositionCapsule::clone() const
+    {
+        return std::unique_ptr<PositionCapsule>(
+            new NonOwningPositionCapsule(buffer.get(), current_size));
+    }
+
+    double*
+    NonOwningPositionCapsule::begin()
+    {
+        raise();
+    }
+
+    const double*
+    NonOwningPositionCapsule::begin() const
+    {
+        return buffer.get();
+    }
+
+    double*
+    NonOwningPositionCapsule::end()
+    {
+        raise();
+    }
+
+    const double*
+    NonOwningPositionCapsule::end() const
+    {
+        return buffer.get() + current_size;
+    }
+
+    const double*
+    NonOwningPositionCapsule::cbegin() const
+    {
+        return cdata();
+    }
+
+    const double*
+    NonOwningPositionCapsule::cend() const
+    {
+        return cdata() + current_size;
+    }
+
+    bool
+    NonOwningPositionCapsule::empty() const
+    {
+        return current_size == 0;
+    }
+
+    std::size_t
+    NonOwningPositionCapsule::size() const
+    {
+        return current_size;
+    }
+
+    std::size_t
+    NonOwningPositionCapsule::nsites() const
+    {
+        return current_size;
+    }
+
+    bool
+    NonOwningPositionCapsule::resizable() const
+    {
+        return false;
+    }
+} // namespace Sequence
+
