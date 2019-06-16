e4241ed84cc504d6ee8f820f65923e6f28556f29
molpopgen <krthornt@uci.edu>
2019-06-16 12:28:24 -0700

    Test window contents

--- test/testVariantMatrixWindows.cc
+++ test/testVariantMatrixWindows.cc
@@ -14,7 +14,22 @@ BOOST_AUTO_TEST_CASE(test_windows)
 {
     std::istringstream i(get_msformat_data());
     auto vm = Sequence::from_msformat(i);
-    Sequence::to_msformat(vm, std::cout);
+    for (double i = 0.0; i < 1.0 - 1e-4; i += 0.1)
+        {
+            auto w = Sequence::make_window(vm, i, i + 0.1);
+            auto pb = std::lower_bound(vm.pbegin(), vm.pend(), i);
+            std::size_t offset = pb - vm.pbegin();
+            for (std::size_t site = 0; site < w.nsites(); ++site)
+                {
+                    auto window_site = Sequence::get_ConstRowView(w, site);
+                    auto matrix_site
+                        = Sequence::get_ConstRowView(vm, offset + site);
+                    auto m
+                        = std::mismatch(window_site.begin(), window_site.end(),
+                                        matrix_site.begin());
+                    BOOST_REQUIRE_EQUAL(m.first == window_site.end(), true);
+                }
+        }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
