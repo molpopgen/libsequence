50c26387676639873c7fa7ba64ca74fcf8c7ddd3
molpopgen <krthornt@uci.edu>
2019-06-17 05:39:16 -0700

    Use msprime data set as basis for new text fixture.

---
+++ test/msprime_data_fixture.hpp
@@ -0,0 +1,25 @@
+#ifndef LIBSEQUENCE_TEST_MSPRIME_DATA_FIXTURE_HPP
+#define LIBSEQUENCE_TEST_MSPRIME_DATA_FIXTURE_HPP
+
+#include <sstream>
+#include "msformatdata.hpp"
+#include <Sequence/VariantMatrix.hpp>
+#include <Sequence/AlleleCountMatrix.hpp>
+#include <Sequence/variant_matrix/msformat.hpp>
+
+struct vmatrix_from_msprime
+{
+    Sequence::VariantMatrix m;
+    Sequence::AlleleCountMatrix c;
+
+    static Sequence::VariantMatrix
+    read()
+    {
+        std::istringstream in(get_msformat_data());
+        return Sequence::from_msformat(in);
+    }
+
+    vmatrix_from_msprime() : m(read()), c(m) {}
+};
+
+#endif
